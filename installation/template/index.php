8cd64fe6e12b4972b4c5b515e5b11b26226351e3
Rouven Weßling <me@rouvenwessling.de>
2012-08-31 18:24:25 +0200

    We don't use popovers in the installation or the login.

--- installation/template/index.php
+++ installation/template/index.php
@@ -86,7 +86,6 @@ JText::script('INSTL_FTP_SETTINGS_CORRECT');
 			function initElements() {
 				(function($){
 					$('*[rel=tooltip]').tooltip()
-					$('*[rel=popover]').popover()
 
 					// Chosen select boxes
 					$("select").chosen({
