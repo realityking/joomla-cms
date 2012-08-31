66df456fd9df0ed336bcc7545d1eac6ae59ee782
Rouven Weßling <me@rouvenwessling.de>
2012-08-31 17:33:07 +0200

    Make the admin layouts a bit more semantic. Coded together with Angie Radtke.

--- administrator/modules/mod_logged/tmpl/default.php
+++ administrator/modules/mod_logged/tmpl/default.php
@@ -12,35 +12,55 @@ defined('_JEXEC') or die;
 
 <div class="row-striped">
 	<?php foreach ($users as $user) : ?>
-		<div class="row-fluid">
-			<div class="span9">
-				<?php if ($user->client_id == 0) :?>
-					<a rel="tooltip" title="<?php echo JText::_('MOD_LOGGED_LOGOUT');?>" href="<?php echo $user->logoutLink;?>" class="btn btn-danger btn-mini">
-						<i class="icon-remove icon-white tip" title="<?php echo JText::_('JLOGOUT');?>"></i>
-					</a>
-				<?php endif; ?>
+		<?php
+			$showLogut = false;
+			if ($user->client_id === 0)
+			{
+				$showLogut = true;
+			}
 
-				<strong class="row-title">
+			$titleSpan = 'span6';
+			if (!$showLogut)
+			{
+				$titleSpan = 'span7';
+			}
+		?>
+		<div class="row-fluid">
+			<dl>
+				<dt class="<?php echo $titleSpan; ?>">
 					<?php if (isset($user->editLink)) :?>
-						<a href="<?php echo $user->editLink; ?>"  rel="tooltip" title="<?php echo JText::_('JGRID_HEADING_ID');?> : <?php echo $user->id; ?>">
+						<a href="<?php echo $user->editLink; ?>" rel="tooltip" title="<?php echo JText::_('JGRID_HEADING_ID');?> : <?php echo $user->id; ?>">
 							<?php echo $user->name;?></a>
 					<?php else :
 						echo $user->name;
 					endif; ?>
-				</strong>
+				</dt>
 
-				<small class="small" rel="tooltip" title="<?php echo JText::_('JCLIENT'); ?>">
-				<?php
-					if($user->client_id) {
-						echo JText::_('JADMINISTRATOR');
-					} else {
-						echo JText::_('JSITE');
-					}?>
-				</small>
-			</div>
-			<div class="span3">
-				<span class="small" rel="tooltip" title="<?php echo JText::_('MOD_LOGGED_LAST_ACTIVITY');?>"><i class="icon-calendar"></i> <?php echo JHtml::_('date', $user->time, 'Y-m-d'); ?></span>
-			</div>
+				<dd class="span2">
+					<small class="small" rel="tooltip" title="<?php echo JText::_('JCLIENT'); ?>">
+					<?php
+						if($user->client_id) {
+							echo JText::_('JADMINISTRATOR');
+						} else {
+							echo JText::_('JSITE');
+						}?>
+					</small>
+				</dd>
+
+
+				<?php if ($showLogut) :?>
+					<dd class="span1">
+						<a rel="tooltip" title="<?php echo JText::_('MOD_LOGGED_LOGOUT');?>" href="<?php echo $user->logoutLink;?>" class="btn btn-danger btn-mini">
+							<i class="icon-remove icon-white tip" title="<?php echo JText::_('JLOGOUT');?>"></i>
+						</a>
+					</dd>
+				<?php endif; ?>
+
+
+				<dd class="span3">
+					<span class="small" rel="tooltip" title="<?php echo JText::_('MOD_LOGGED_LAST_ACTIVITY');?>"><i class="icon-calendar"></i> <?php echo JHtml::_('date', $user->time, 'Y-m-d'); ?></span>
+				</dd>
+			</dl>
 		</div>
 	<?php endforeach; ?>
 </div>
