a11e5c97d3a930c6f4eafe197510f1ca43dd79c9
Rouven Weßling <me@rouvenwessling.de>
2012-08-31 17:36:23 +0200

    We always want the lang, xml:lang, dir and xmlns attributes on the html element.

--- administrator/templates/isis/component.php
+++ administrator/templates/isis/component.php
@@ -31,7 +31,7 @@ endif;
 
 ?>
 <!DOCTYPE html>
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $this->language; ?>" lang="<?php echo $this->language; ?>" >
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $this->language; ?>" lang="<?php echo $this->language; ?>" dir="<?php echo $this->direction; ?>">
 <head>
 	<script src="<?php echo JUri::root(); ?>media/jui/js/jquery.min.js"></script>
 	<script src="<?php echo JUri::root(); ?>media/jui/js/bootstrap.min.js"></script>
--- administrator/templates/isis/login.php
+++ administrator/templates/isis/login.php
@@ -31,7 +31,7 @@ $itemid   = $app->input->getCmd('Itemid', '');
 $sitename = $app->getCfg('sitename');
 ?>
 <!DOCTYPE html>
-<html>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $this->language; ?>" lang="<?php echo $this->language; ?>" dir="<?php echo $this->direction; ?>">
 <head>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
